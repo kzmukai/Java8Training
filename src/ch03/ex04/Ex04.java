7d09ace3f1213853a559845cda91a289fb162914
kazuhiro <kazuhiro.mukai@nts.ricoh.co.jp>
2016-12-12 17:33:58 +0900

    ad

---
+++ src/ch03/ex04/Ex04.java
@@ -0,0 +1,36 @@
+package ch03.ex04;
+
+/**
+ * 名前の中にFilterを含む関数型インタフェースが、Java API にはいくつありますか。 そのうちのどれが、Predicate
+ * <T>よりも付加価値がありますか。
+ * 
+ */
+public class Ex04 {
+
+	//IF
+ 
+	//java.nio.file.DirectoryStream.Filter<T>
+
+	//javax.xml.stream.EventFilter
+	
+	//java.io.FileFilter
+	
+	//java.io.FilenameFilter	
+	
+	//java.util.logging.Filter
+	
+	//javax.management.NotificationFilter
+	
+	//javax.imageio.spi.ServiceRegistry.Filter
+	
+	//javax.xml.stream.StreamFilter
+	
+    //Predicate
+	// <T>よりも付加価値がありますか。
+	//java.nio.file.DirectoryStream.Filter<T>
+	//例外をスローする
+	//java.io.FilenameFilter
+	//引数が2つある
+ 
+	
+}
