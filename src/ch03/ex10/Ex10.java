de498adb1485b9eb45e2f964d2f082a4ab544b6c
kazuhiro <kazuhiro.mukai@nts.ricoh.co.jp>
2016-12-14 19:36:55 +0900

    ad

--- src/ch03/ex10/Ex10.java
+++ src/ch03/ex10/Ex10.java
@@ -1,8 +1,9 @@
 package ch03.ex10;
+
 /**
- * 
- * なぜ、次の呼び出しができないのでしょうか。
+ * 。
  *
+ * なぜ、次の呼び出しができないのでしょうか
  * UnaryOperator<T>のcompose メソッドの戻り値型を注意深く調べなさい。なぜ、 transform
  * メソッドに対しては、適切ではないのでしょうか。関数合成に関しては、ス トラクチャル型（structural type）とノミナル型（nominal
  * type）*4のユーティリティに関 して、何が言えますか。
@@ -10,5 +11,17 @@ package ch03.ex10;
  *
  */
 public class Ex10 {
-
+	
+	// なぜ、次の呼び出しができないのでしょうか
+	//Color::brighterの型はUnaryOperator<Color>、Color::grayscaleの型もUnaryOperator<Color>である
+	//しかしUnaryOperator<T>のスーパーインターフェースのFunction<T,T>の default メソッドcomposeの戻り値の型はFunction<Color, Color>になる
+	//ためビルドエラー
+	
+	
+	//ransform メソッドに対しては、適切ではないのでしょうか。
+	//?
+	
+	//関数合成に関しては、ス トラクチャル型（structural type）とノミナル型（nominaltype）*4のユーティリティに関 して、何が言えますか。
+	//?
+	
 }
