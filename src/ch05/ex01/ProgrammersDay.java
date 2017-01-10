b31c277f7ba86c4ebf8a2c7ac8e2441a2bd8f5d5
kazuhiro <kazuhiro.mukai@nts.ricoh.co.jp>
2017-01-10 09:27:40 +0900

    ad5-1

---
+++ src/ch05/ex01/ProgrammersDay.java
@@ -0,0 +1,18 @@
+package ch05.ex01;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * plusDayを使用しないで, プログラマーの日を計算しなさい.
+ *
+ * テストはコンソールの結果を目視で確認する。
+ */
+public class ProgrammersDay {
+
+	public static void main(String[] args) {
+		LocalDate programmersDay = LocalDate.of(2015, 1, 1).plus(255, ChronoUnit.DAYS);
+		System.out.println(programmersDay);
+	}
+
+}
