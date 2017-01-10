3abb6679d64f3548233f5e79bdf1716fec99ccf2
kazuhiro <kazuhiro.mukai@nts.ricoh.co.jp>
2017-01-10 12:38:59 +0900

    ad 5-2

--- src/ch05/ex02/AddingYears.java
+++ src/ch05/ex02/AddingYears.java
@@ -1,12 +1,22 @@
 package ch05.ex02;
 
-/** 
-* 2. LocalDate.of(2000, 2, 29)に1年を加算すると何が起きますか. 
-*さらに, 1年を4回加算するとどうなりますか.
-*
-**/ 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
+/**
+ *LocalDate.of(2000, 2, 29)に1年を加算すると何が起きますか. さらに, 1年を4回加算するとどうなりますか.
+ *
+ *テストはコンソールを目視で確認
+ **/
 
 public class AddingYears {
 
+	public static void main(String[] args) {
+		LocalDate addingOneYears = LocalDate.of(2000, 2, 29).plusYears(1);
+		System.out.println(addingOneYears);
+
+		LocalDate addingFourYears = LocalDate.of(2000, 2, 29).plusYears(4);
+		System.out.println(addingFourYears);
+
+	}
 }
