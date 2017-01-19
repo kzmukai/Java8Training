071eaff15b32d86b2afe407941d344f0c712b76c
kazuhiro <kazuhiro.mukai@nts.ricoh.co.jp>
2017-01-19 09:38:46 +0900

    add

--- src/ch06/ex04/LongUtil.java
+++ src/ch06/ex04/LongUtil.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.LongAccumulator;
 
+
 public class LongUtil {
 	
 	public static void main(String[] args) throws InterruptedException {
---
+++ src/ch06/ex07/MaxKeySercer.java
@@ -0,0 +1,19 @@
+package ch06.ex07;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+/**
+ * @author budougumi0617
+ * @note ConcurrentHashMap<String,Long>内で、
+ *       最大値を持つキーを見つけなさい
+ *       （同じ最大値を持つキーがあれば、どちらのキーでも構いません）。
+ */
+public class MaxKeySercer {
+	public static Entry<String, Long> resultEntries;
+
+	public static String getKeyHasMaxValue(ConcurrentHashMap<String, Long> map) {
+		resultEntries = map.reduceEntries(1, (entry1, entry2) -> entry1.getValue()
+				.compareTo(entry2.getValue()) > 0 ? entry1 : entry2);
+		return resultEntries.getKey();
+	}
+}
+
